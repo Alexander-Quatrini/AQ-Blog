f00f61c4d22d5ff5090180b8c9a5f314f525419a
quatts <alexanderquatrini@gmail.com>
2021-02-23 20:16:13 -0600

    add intro and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
