a2080a8a0f605211ef027d76068ca7edb163e9ff
Alexander Quatrini <alexanderquatrini@gmail.com>
2021-02-24 15:00:19 -0600

    added nodejs server

---
+++ nodejsAPI/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express(),
+    bodyParser = require("body-parser");
+    port = 3080;
+
+const posts = [];
+
+app.use(bodyParser.json());
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+  });
+  
+  app.post('/api/post', (req, res) => {
+    const post = req.body.post;
+    posts.push(post);
+    res.json("post added");
+  });
+  
+  app.get('/', (req,res) => {
+      res.send('App Works !!!!');
+  });
+  
+  app.listen(port, () => {
+      console.log(`Server listening on the port::${port}`);
+  });
+  
