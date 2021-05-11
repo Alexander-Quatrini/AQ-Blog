98a5ca0fdf2147785c5d1e3b5ddd2c2964992111
quatts <alexanderquatrini@gmail.com>
2021-05-11 14:55:00 -0500

    working on loading screens

--- src/app/blog-post-view/blog-post/blog-post.component.ts
+++ src/app/blog-post-view/blog-post/blog-post.component.ts
@@ -1,8 +1,8 @@
-import { Component, Injectable, Input, OnInit } from '@angular/core';
+import { Component, Injectable, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { finalize, switchMap, take } from 'rxjs/operators';
 import { BlogDataService, BlogPostContent } from 'src/app/blog-data.service';
 
 @Component({
@@ -11,21 +11,29 @@ import { BlogDataService, BlogPostContent } from 'src/app/blog-data.service';
   styleUrls: ['./blog-post.component.sass']
 })
 @Injectable()
-export class BlogPostComponent implements OnInit {
+export class BlogPostComponent implements OnInit, OnDestroy {
 
+  blogPostDataSubscription!: Subscription;
   blogPostData$!: Observable<BlogPostContent[]>;
-
+  relatedPostsSubscription!: Subscription;
   relatedPosts$!: Observable<BlogPostContent[]>;
 
   selectedId!: string;
   type!: string;
 
+  loading: boolean = true
+  loadingRelated: boolean = true
+
   constructor(public router:Router, private http: HttpClient, private dataService: BlogDataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.blogPostDataSubscription = this.route.params.pipe(finalize(() => {this.loading = false;})).subscribe(data => {this.selectedId = data.id; this.blogPostData$ = this.dataService.fetchBlog(this.selectedId);})
+    this.relatedPostsSubscription = this.route.params.pipe(finalize(() => {this.loadingRelated = false;})).subscribe(data => {this.type = data.type; this.relatedPosts$ = this.dataService.fetchRelatedArticles(this.type);})
+  }
 
-    this.route.params.subscribe(data => {this.selectedId = data.id; this.blogPostData$ = this.dataService.fetchBlog(this.selectedId);})
-    this.route.params.subscribe(data => {this.type = data.type; this.relatedPosts$ = this.dataService.fetchRelatedArticles(this.type);})
+  ngOnDestroy(): void{
+    this.blogPostDataSubscription.unsubscribe();
+    this.relatedPostsSubscription.unsubscribe();
   }
 
 title: string ="";
